97932c9a230e8efa0db4ce71ffb773137ed46108
i.fedorova <i.fedorova@bftcom.com>
2020-04-07 21:30:43 +0300

    add new math operators and test

--- src/lesson2/mathOperators.test.ts
+++ src/lesson2/mathOperators.test.ts
@@ -1,4 +1,4 @@
-import { mul, div, add, minus } from "./mathOperators";
+import { mul, div, add, minus, squaring, exponentiation, factorial } from "./mathOperators";
 
 describe("mathOperators test cases", () => {
   it("mul 1 * 2 to equal 2", () => {
@@ -23,5 +23,29 @@ describe("mathOperators test cases", () => {
 
   it("minus 4 - 2 to equal 2", () => {
     expect(minus(4, 2)).toBe(2);
+  });
+
+  it("squaring 2** to equal 4", () => {
+    expect(squaring(2)).toBe(4);
+  });
+
+  it("squaring 5** to equal 25", () => {
+    expect(squaring(5)).toBe(25);
+  });
+
+  it("exponentiation 2 ^ 3 to equal 8", () => {
+    expect(exponentiation(2, 3)).toBe(8);
+  });
+
+  it("exponentiation 4 ^ 5 to equal 1024", () => {
+    expect(exponentiation(2, 2)).toBe(4);
+  });
+
+  it("factorial 6! to equal 720", () => {
+    expect(factorial(6)).toBe(720);
+  });
+
+  it("factorial 4! to equal 24", () => {
+    expect(factorial(4)).toBe(24);
   });
 });
--- src/lesson2/mathOperators.ts
+++ src/lesson2/mathOperators.ts
@@ -1,4 +1,10 @@
 export type ScalarOperationType = (first: number, second: number) => number;
+export type UnoOperationType = (first: number) => number;
+
+const getFactorial = (accumulator: number, val: number): number =>
+  val == 1 ? accumulator : getFactorial(accumulator * val, val - 1);
+
+const factorialRec = (num: number): number => getFactorial(1, num);
 
 export const mul: ScalarOperationType = (
   first: number,
@@ -20,11 +26,27 @@ export const minus: ScalarOperationType = (
   second: number
 ): number => first - second;
 
-export const mathOperators: { [key: string]: ScalarOperationType } = {
+export const squaring: UnoOperationType = (first: number): number =>
+  (first **= 2);
+
+export const exponentiation: ScalarOperationType = (
+  first: number,
+  second: number
+): number => first ** second;
+
+export const factorial: UnoOperationType = (first: number): number =>
+  factorialRec(first);
+
+export const mathOperators: {
+  [key: string]: ScalarOperationType | UnoOperationType;
+} = {
   "*": mul,
   "/": div,
   "+": add,
   "-": minus,
+  "**": squaring,
+  "^": exponentiation,
+  "!": factorial,
 };
 
 export const mathPriorities: number[] = [1, 2];
@@ -34,6 +56,9 @@ const [FIRST, SECOND] = mathPriorities;
 export const mathOperatorsPriorities: { [key: string]: number } = {
   "*": FIRST,
   "/": FIRST,
+  "**": FIRST,
+  "^": FIRST,
+  "!": FIRST,
   "+": SECOND,
   "-": SECOND,
 };
