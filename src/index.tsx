1080e69fdf75227b4c6f35ef4b0fef0052903d30
i.fedorova <i.fedorova@bftcom.com>
2020-04-11 19:31:05 +0300

    add component hellouser

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 
 import { ClickCounter } from "./ClickCounter";
+import { HelloUser } from "./HelloUser";
 
 render(<ClickCounter start={1} />, document.getElementById("root"));
