16aa38ab3822054aee6a8b6f0b107b53ef19140d
i.fedorova <i.fedorova@bftcom.com>
2020-04-18 17:43:04 +0300

    new structure project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { ClickCounter } from "./ClickCounter";
-import { HelloUser } from "./HelloUser";
+import { ClickCounter } from "./lesson3/ClickCounter";
+import { HelloUser } from "./lesson3/HelloUser";
 
 render(<ClickCounter start={1} />, document.getElementById("root"));
