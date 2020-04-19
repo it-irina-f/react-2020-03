8141dce485313d0168f623fd7c363b034e36f2fd
i.fedorova <i.fedorova@bftcom.com>
2020-04-19 22:13:10 +0300

    add test for component listItem

--- src/components/ToDoList/components/List/components/ListItem/ListItem.test.tsx
+++ src/components/ToDoList/components/List/components/ListItem/ListItem.test.tsx
@@ -3,3 +3,42 @@ import { mount } from "enzyme";
 import renderer from "react-test-renderer";
 
 import { ListItem } from "./ListItem";
+
+
+const listItem1 = { id: 0, text: "Полить цветы", isComplete: true };
+const listItem2 = { id: 1, text: "Сделать ДЗ", isComplete: false };
+
+const click = jest.fn();
+
+describe("Component ListItem", () => {
+  // const item = render(<ListItem listItem={listItem1} toggleComplete={click} />);
+  it("renders list item where isComplete=true", () => {
+    expect(
+      renderer
+        .create(<ListItem listItem={listItem1} toggleComplete={click} />)
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it("renders list item where isComplete=false", () => {
+    expect(
+      renderer
+        .create(<ListItem listItem={listItem2} toggleComplete={click} />)
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+  it("simulate onChange checkbox by list item where isComplete=true", () => {
+    const wrapper = mount(
+      <ListItem listItem={listItem1} toggleComplete={click} />
+    );
+    wrapper.find("input").simulate("change");
+    expect(click).toHaveBeenCalled();
+  });
+  it("simulate onChange checkbox by list item where isComplete=false", () => {
+    const wrapper = mount(
+      <ListItem listItem={listItem2} toggleComplete={click} />
+    );
+    wrapper.find("input").simulate("change");
+    expect(click).toHaveBeenCalled();
+  });
+});
