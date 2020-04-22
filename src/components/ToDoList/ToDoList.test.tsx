c9c65d8dc19f321399ddcadb3f7f40d5c9b637b7
i.fedorova <i.fedorova@bftcom.com>
2020-04-22 23:24:22 +0300

    update test

--- src/components/ToDoList/ToDoList.test.tsx
+++ src/components/ToDoList/ToDoList.test.tsx
@@ -6,8 +6,18 @@ import { ToDoList } from "./ToDoList";
 
 const click = jest.fn();
 
+const list = [
+  { id: 0, text: "Полить цветы", isComplete: true },
+  { id: 1, text: "Сделать ДЗ", isComplete: false },
+  { id: 2, text: "Купить продукты", isComplete: true },
+  { id: 3, text: "Приготовить ужин", isComplete: false },
+  { id: 4, text: "Уборка квартиры", isComplete: false },
+];
+
 describe("Component ToDoList", () => {
   it("renders list", () => {
-    expect(renderer.create(<ToDoList />).toJSON()).toMatchSnapshot();
+    expect(
+      renderer.create(<ToDoList list={list} />).toJSON()
+    ).toMatchSnapshot();
   });
 });
