5735408286cd646c72c742094f397215222b1be1
i.fedorova <i.fedorova@bftcom.com>
2020-07-07 22:15:31 +0300

    add User test

---
+++ src/components/User/User.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import { ToDoState } from "@/AppStore";
+import { User } from "./User";
+
+const store = configureMockStore<ToDoState>([])({
+  login: {
+    status: 1,
+    username: "Irina",
+  },
+});
+
+describe("User Test", () => {
+  const screen = mount(
+    <Provider store={store}>
+      <User />
+    </Provider>
+  );
+
+  it("should render username from store", () => {
+    expect(screen.text().includes("Irina")).toBe(true);
+  });
+
+  it("should render button for logout", () => {
+    expect(screen.find("button").text().includes("Выход")).toBe(true);
+  });
+
+  it("should call logout when click on button", () => {
+    screen.find("Button").simulate("click");
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({ type: "user/logout", payload: undefined });
+  });
+});
