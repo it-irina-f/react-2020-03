ba1466f5a3ab9653633b7fdd06df80df1da6f061
i.fedorova <i.fedorova@bftcom.com>
2020-09-06 21:29:18 +0300

    refactor saga for Auth

--- src/modules/Auth/saga.ts
+++ src/modules/Auth/saga.ts
@@ -1,34 +1,39 @@
 import { isEmpty } from "ramda";
-import { takeEvery, call, put, fork } from "redux-saga/effects";
+import { take, call, put, fork } from "redux-saga/effects";
 
 import { getUserSession, login, logout } from "@/api/auth";
 
-import { actions } from "./reducer";
-
-export function* checkUserSession() {
-  const userSession = yield call(getUserSession);
-  if (userSession && !isEmpty(userSession)) {
-    yield put(actions.login(userSession));
-  } else {
-    yield put(actions.logout());
-  }
-}
+import { actions, usernameMinLength } from "./reducer";
 
 export function* clearUserSession() {
   yield call(logout);
+}
+
+export function* checkUserSession() {
+  const userSession: string = yield call(getUserSession);
+  if (userSession?.length > usernameMinLength && !isEmpty(userSession)) {
+    yield put(actions.login(userSession));
+  } else {
+    yield put(actions.logout());
+    yield* clearUserSession();
+  }
 }
 
 export function* saveUserSession({
   payload,
 }: ReturnType<typeof actions.login>) {
   const username = String(payload);
-  if (username && !isEmpty(username)) {
+  if (username?.length > usernameMinLength && !isEmpty(username)) {
     yield call(login, username);
   }
 }
 
 export function* authSaga() {
   yield fork(checkUserSession);
-  yield takeEvery(actions.login.type, saveUserSession);
-  yield takeEvery(actions.logout.type, clearUserSession);
+  while (true) {
+    const action = yield take(actions.login.type);
+    yield* saveUserSession(action);
+    yield take(actions.logout.type);
+    yield* clearUserSession();
+  }
 }
