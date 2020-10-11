0e694ccf57e20ce0f10317f99802dcb5688eeb77
i.fedorova <i.fedorova@bftcom.com>
2020-10-11 13:29:13 +0300

    issues#21

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { IconEdit, IconTrash2 } from "sancho";
+import { IconEdit, IconTrash2, Text } from "sancho";
 import styled from "@emotion/styled";
 import { ManageButton } from "@/components/Buttons";
+import { number } from "@storybook/addon-knobs";
 
 interface ListItemProps {
   id: number;
@@ -20,6 +21,7 @@ interface Props {
 
 const ListItemWrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
   margin: 10px auto;
   border-bottom: 1px solid #ddd;
   padding: 0 0 10px;
@@ -29,14 +31,33 @@ const LabelWrapper = styled.label`
   flex-grow: 1;
 `;
 
+const TextWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
 export const ListItem: React.FC<Props> = ({
   listItem,
   toggleComplete,
   deleteListItem,
   handleEdit,
 }) => {
+  const date = new Date(listItem.id);
   return (
     <ListItemWrapper>
+      <TextWrapper>
+        <Text variant="subtitle">
+          {date.getDate() +
+            "." +
+            (date.getMonth() + 1) +
+            "." +
+            date.getFullYear() +
+            " " +
+            date.getHours() +
+            ":" +
+            date.getMinutes()}
+        </Text>
+      </TextWrapper>
       <LabelWrapper>
         <input
           type="checkbox"
